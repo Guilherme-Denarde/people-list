f9e6c88c154046af4bd16a5609589f728f906eba
Guilh <wanderfull.mystc@gmail.com>
2023-10-03 23:26:25 -0300

    teste

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonComponent } from './layout/person/person.component';
+import { IndexComponent } from './layout/index/index.component';
+import { HeaderComponent } from './layout/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonComponent,
+    IndexComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
